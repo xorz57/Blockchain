09247b17323c07076d790bf4beeaadbac30ea1cd
George Fotopoulos <xorz57@gmail.com>
2024-06-28 00:40:22 +0300

    Update

--- src/Block.cpp
+++ src/Block.cpp
@@ -3,13 +3,25 @@
 
 #include <openssl/sha.h>
 
+#include <array>
 #include <ctime>
+#include <format>
 #include <iomanip>
 #include <iostream>
 #include <mutex>
 #include <sstream>
 #include <thread>
 #include <vector>
+
+std::string sha256(const std::string &input) {
+    std::ostringstream oss;
+    std::array<unsigned char, SHA256_DIGEST_LENGTH> bytes;
+    SHA256(static_cast<const unsigned char *>(static_cast<const void *>(input.c_str())), input.size(), bytes.data());
+    for (const auto &byte: bytes) {
+        oss << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(byte);
+    }
+    return oss.str();
+}
 
 block_t::block_t(std::uint32_t index, std::string data, std::string hash_prev) : index(index),
                                                                                  data(data),
@@ -20,18 +32,9 @@ block_t::block_t(std::uint32_t index, std::string data, std::string hash_prev) :
 
 std::string block_t::hash() const {
     std::ostringstream oss;
-
     oss << timestamp << nonce << index << data << hash_prev;
     std::string buffer = oss.str();
-
-    oss.str("");
-
-    std::vector<unsigned char> hash_bytes(SHA256_DIGEST_LENGTH);
-    SHA256(static_cast<const unsigned char *>(static_cast<const void *>(buffer.c_str())), buffer.size(), hash_bytes.data());
-    for (const auto &byte: hash_bytes) {
-        oss << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(byte);
-    }
-    return oss.str();
+    return sha256(buffer);
 }
 
 void block_t::mine(std::uint32_t difficulty) {
@@ -41,7 +44,7 @@ void block_t::mine(std::uint32_t difficulty) {
     std::mutex mutex;
     std::string str(difficulty, '0');
 
-    auto start_time = std::chrono::steady_clock::now();
+    auto t0 = std::chrono::steady_clock::now();
 
     auto mining_thread = [&](int thread_id) {
         while (!found) {
@@ -66,16 +69,15 @@ void block_t::mine(std::uint32_t difficulty) {
         }
     }
 
-    auto end_time = std::chrono::steady_clock::now();
+    auto t1 = std::chrono::steady_clock::now();
 
-    std::chrono::duration<double> elapsed_seconds = end_time - start_time;
+    std::chrono::duration<double> duration = t1 - t0;
 
-    int hours = std::chrono::duration_cast<std::chrono::hours>(elapsed_seconds).count();
-    int minutes = std::chrono::duration_cast<std::chrono::minutes>(elapsed_seconds).count() % 60;
-    int seconds = std::chrono::duration_cast<std::chrono::seconds>(elapsed_seconds).count() % 60;
+    int hours = std::chrono::duration_cast<std::chrono::hours>(duration).count();
+    int minutes = std::chrono::duration_cast<std::chrono::minutes>(duration).count() % 60;
+    int seconds = std::chrono::duration_cast<std::chrono::seconds>(duration).count() % 60;
 
-    std::cout << color::basic::green
-              << "mined block #" << index << " " << hash_curr << " in "
-              << hours << " hours " << minutes << " minutes " << seconds << " seconds"
-              << color::reset << std::endl;
+    std::cout << color::basic::green;
+    std::cout << std::format("mined block #{} {} in {} hours {} minutes {} seconds", index, hash_curr, hours, minutes, seconds);
+    std::cout << color::reset << std::endl;
 }
