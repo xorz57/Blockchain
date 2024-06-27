940682a7f9059e188dbf2126d1ebc7307e7354c1
George Fotopoulos <xorz57@gmail.com>
2024-06-27 22:07:02 +0300

    Update

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,20 +5,10 @@
 #include <iostream>
 
 int main() {
-    blockchain_t blockchain(4);
+    blockchain_t blockchain(6);
 
-    blockchain.push(block_t(blockchain.blocks.back().index + 1, "block #1", blockchain.blocks.back().hash_curr));
-    blockchain.push(block_t(blockchain.blocks.back().index + 1, "block #2", blockchain.blocks.back().hash_curr));
-    blockchain.push(block_t(blockchain.blocks.back().index + 1, "block #3", blockchain.blocks.back().hash_curr));
-
-    if (blockchain.is_valid()) {
-        std::cout << color::basic::white << "================================================================================" << color::reset << std::endl;
-        std::cout << color::basic::green << "Blockchain is valid!" << color::reset << std::endl;
-        std::cout << color::basic::white << "================================================================================" << color::reset << std::endl;
-    } else {
-        std::cout << color::basic::white << "================================================================================" << color::reset << std::endl;
-        std::cerr << color::basic::red << "Blockchain is invalid!" << color::reset << std::endl;
-        std::cout << color::basic::white << "================================================================================" << color::reset << std::endl;
+    while (blockchain.is_valid()) {
+        blockchain.push(block_t(blockchain.blocks.back().index + 1, "", blockchain.blocks.back().hash_curr));
     }
 
     return 0;
