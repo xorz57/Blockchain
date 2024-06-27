d97530d38e12c4474a1526232350fe0fe6330f75
George Fotopoulos <xorz57@gmail.com>
2024-06-28 01:05:52 +0300

    Update

--- src/Block.hpp
+++ src/Block.hpp
@@ -1,10 +1,13 @@
 #pragma once
 
 #include <cstdint>
+#include <ostream>
 #include <string>
 
 struct block_t {
     block_t(std::uint32_t index, std::string data, std::string hash_prev);
+
+    friend std::ostream &operator<<(std::ostream &os, const block_t &block);
 
     std::string hash() const;
     void mine(std::uint32_t difficulty);
