65a067fb9860322127c77f8d9cc859578a0a5d93
George Fotopoulos <xorz57@gmail.com>
2024-06-29 02:12:23 +0300

    Update

--- src/Main.cpp
+++ src/Main.cpp
@@ -32,7 +32,7 @@ std::vector<transaction_t> generate_random_transactions() {
 }
 
 int main() {
-    auto blockchain = blockchain_t(5);
+    auto blockchain = blockchain_t(6);
 
     std::cout << blockchain.blocks.front() << std::endl;
 
