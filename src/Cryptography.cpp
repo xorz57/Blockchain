63995af1048de216883b81706ac83d3b24bbf29f
George Fotopoulos <xorz57@gmail.com>
2024-06-29 03:41:38 +0300

    Revert "Fix types"
    This reverts commit e0e120c3f743ff3b26440c06afc3a996ec886396.

--- src/Cryptography.cpp
+++ src/Cryptography.cpp
@@ -8,9 +8,9 @@
 
 std::string cryptography::sha256(const std::string &input) {
     std::ostringstream oss;
-    std::array<std::uint8_t, SHA256_DIGEST_LENGTH> bytes;
-    SHA256(static_cast<const std::uint8_t *>(static_cast<const void *>(input.c_str())), input.size(), bytes.data());
-    for (const std::uint8_t byte: bytes) {
+    std::array<unsigned char, SHA256_DIGEST_LENGTH> bytes;
+    SHA256(static_cast<const unsigned char *>(static_cast<const void *>(input.c_str())), input.size(), bytes.data());
+    for (const std::uint32_t byte: bytes) {
         oss << std::hex << std::setw(2) << std::setfill('0') << byte;
     }
     return oss.str();
